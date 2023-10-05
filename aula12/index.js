7e7db58d599b3c0528005b1411bb5d36f0830a25
Irina-Chang <atenafreak@gmail.com>
2023-10-04 21:20:58 -0300

    native resources

---
+++ aula12/index.js
@@ -0,0 +1,20 @@
+let nome = "Irina"
+
+console.log(nome)
+console.error(nome)
+console.warn(nome)
+
+
+//window.alert(nome)
+/*nome = window.prompt("Insira o seu nome: ")
+console.log(nome)*/
+
+let confirmacao = window.confirm("Confirma essa ação?")
+console.log(confirmacao)
+
+
+//pode-se utilizar os atalhos sem a necessidade de por o .windows
+alert("Olá Vida!")
+prompt("Insira seu nome: ")
+document //faz referencia ao arquivo html
+
