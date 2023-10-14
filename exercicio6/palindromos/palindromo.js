9e18372b8bdc0bee21cfa8876303025cb690daa0
Irina-Chang <atenafreak@gmail.com>
2023-10-14 00:34:43 -0300

    correcao exercicio tabuada

---
+++ exercicio6/palindromos/palindromo.js
@@ -0,0 +1,10 @@
+let palavra = prompt("Está palavra é um palíndromo?" + "Insira a palavra: ")
+
+palindromo = " "
+
+indice = 1
+for ( ; ; ){
+
+}
+
+
--- exercicio6/roboTabuada/tabuada.js
+++ exercicio6/roboTabuada/tabuada.js
@@ -1,9 +1,17 @@
 let numero = parseFloat(prompt("Insira o número a ser multiplicado:"))
 let tabuada = " "
 
+/* 
 let indice = 1
 for(indice > 0; indice <= 20 ; indice++ ){
     let produto = parseFloat(indice*numero)
     tabuada += produto + "\n"
 }
 alert("A tabuada de 1 a 20 do número " + numero + " é:" + ("\n") + tabuada)
+*/
+//pode colocar a variável dentro do for:
+
+for (let indice =1; indice <= 20 ; indice++){
+    tabuada += " -> " + numero + " x " + indice + " = " + (numero * indice) + "\n"
+}
+alert("Resultado da Tabuada de " + numero + ":\n\n" + tabuada)
