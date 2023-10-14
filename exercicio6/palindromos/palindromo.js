a6b03604474bd7c8cafeee7afc82ab18f1ac6e62
Irina-Chang <atenafreak@gmail.com>
2023-10-14 02:14:24 -0300

    correcao palindromo

--- exercicio6/palindromos/palindromo.js
+++ exercicio6/palindromos/palindromo.js
@@ -1,10 +1,12 @@
-let palavra = prompt("Está palavra é um palíndromo?" + "Insira a palavra: ")
+let palavra = prompt("Está palavra é um palíndromo? \n" + "Insira a palavra: ")
 
-palindromo = " "
-
-indice = 1
-for ( ; ; ){
-
+let palindromo = ""
+for (let i = palavra.length -1; i >= 0; i--){
+    palindromo += palavra[i]
 }
 
-
+if (palavra === palindromo) {
+    alert(palavra + " é um palíndromo!")
+  } else {
+    alert(palavra + " não é um palíndromo!\n\n" + palavra + " !== " + palindromo)
+  }
